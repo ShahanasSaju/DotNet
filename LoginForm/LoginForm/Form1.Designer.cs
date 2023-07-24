2f851a9ddaad7e9c49418730085b403ee8a4cafb
shehnaz <shehnaz@42square.ai>
2023-07-24 23:36:55 +0530

    login form

---
+++ LoginForm/LoginForm/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace LoginForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PasswordValidation = new System.Windows.Forms.Label();
+            this.message = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.loginbutton = new System.Windows.Forms.Button();
+            this.alert = new System.Windows.Forms.Label();
+            this.alert1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.alert1);
+            this.panel1.Controls.Add(this.alert);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.PasswordValidation);
+            this.panel1.Controls.Add(this.message);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Password);
+            this.panel1.Controls.Add(this.Username);
+            this.panel1.Controls.Add(this.loginbutton);
+            this.panel1.Location = new System.Drawing.Point(162, 109);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(361, 224);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(162, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 8;
+            // 
+            // PasswordValidation
+            // 
+            this.PasswordValidation.AutoSize = true;
+            this.PasswordValidation.ForeColor = System.Drawing.Color.Red;
+            this.PasswordValidation.Location = new System.Drawing.Point(140, 114);
+            this.PasswordValidation.Name = "PasswordValidation";
+            this.PasswordValidation.Size = new System.Drawing.Size(0, 16);
+            this.PasswordValidation.TabIndex = 7;
+            this.PasswordValidation.Visible = false;
+            // 
+            // message
+            // 
+            this.message.AutoSize = true;
+            this.message.ForeColor = System.Drawing.Color.Red;
+            this.message.Location = new System.Drawing.Point(97, 208);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(0, 16);
+            this.message.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(138, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Login";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "User Name";
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(146, 89);
+            this.Password.Name = "Password";
+            this.Password.PasswordChar = '.';
+            this.Password.Size = new System.Drawing.Size(100, 22);
+            this.Password.TabIndex = 2;
+            this.Password.TextChanged += new System.EventHandler(this.Password_TextChanged);
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(146, 44);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(100, 22);
+            this.Username.TabIndex = 1;
+            this.Username.TextChanged += new System.EventHandler(this.Username_TextChanged);
+            // 
+            // loginbutton
+            // 
+            this.loginbutton.Location = new System.Drawing.Point(128, 150);
+            this.loginbutton.Name = "loginbutton";
+            this.loginbutton.Size = new System.Drawing.Size(75, 35);
+            this.loginbutton.TabIndex = 0;
+            this.loginbutton.Text = "Login";
+            this.loginbutton.UseVisualStyleBackColor = true;
+            this.loginbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // alert
+            // 
+            this.alert.AutoSize = true;
+            this.alert.Location = new System.Drawing.Point(136, 130);
+            this.alert.Name = "alert";
+            this.alert.Size = new System.Drawing.Size(0, 16);
+            this.alert.TabIndex = 9;
+            // 
+            // alert1
+            // 
+            this.alert1.AutoSize = true;
+            this.alert1.Location = new System.Drawing.Point(37, 130);
+            this.alert1.MinimumSize = new System.Drawing.Size(300, 0);
+            this.alert1.Name = "alert1";
+            this.alert1.Size = new System.Drawing.Size(300, 16);
+            this.alert1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.Button loginbutton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label message;
+        private System.Windows.Forms.Label PasswordValidation;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label alert;
+        private System.Windows.Forms.Label alert1;
+    }
+}
+
---
+++ LoginForm/LoginForm/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user, password;
+            user = Username.Text;
+            password = Password.Text;
+            if (user == "admin" && password == "p@ss123")
+            {
+                message.Text = "Successfully logged in as Admin";
+                message.ForeColor = System.Drawing.Color.Green;
+            }
+            else if(user != "admin" && password != "p@ss123")
+            {
+               alert1.Text = "User not Registered. Please Register !";
+                alert1.ForeColor = System.Drawing.Color.Red;
+                message.Visible = false;
+            }
+            else if(password.Length < 6)
+            {
+                alert1.Text = "Password must be atleast 6 characters !";
+                alert1.ForeColor = System.Drawing.Color.Red;
+                message.Visible = false;
+            }
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+    }
+}
