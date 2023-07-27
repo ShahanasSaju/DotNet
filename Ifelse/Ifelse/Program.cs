90d5aed88eb8fbed18edb4083211731327163d1e
shehnaz <shehnaz@42square.ai>
2023-07-27 20:40:01 +0530

    if else program

---
+++ Ifelse/Ifelse/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Ifelse
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int n = int.Parse(Console.ReadLine());
+            if(n == 1)
+            {
+                Console.WriteLine("Today is monday");
+            }
+            else if(n == 2)
+                {
+                Console.WriteLine("Today is tuesday");
+            }
+            else if( n == 3)
+            {
+                Console.WriteLine("Today is wednesday");
+            }
+            else if(n == 4)
+            {
+                Console.WriteLine("Today is wednesday");
+            }
+            else if (n == 4)
+            {
+                Console.WriteLine("Today is thursday");
+            }
+            else if (n == 5)
+            {
+                Console.WriteLine("Today is friday");
+            }
+            else if (n == 6)
+            {
+                Console.WriteLine("Today is saturday");
+            }
+            else if(n == 7)
+                {
+                Console.WriteLine("Today is sunday");
+            }
+            else
+            {
+                Console.WriteLine("Have a good day");
+            }
+        }
+    }
+}
